ac79de5bb05cd0bbd5c48b4d271558092f2910c2
Satya Prakash <143164106+satya643@users.noreply.github.com>
2024-10-12 08:47:31 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -5,8 +5,8 @@ const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
 
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
         });
         console.log('DB Connected Successfully');
     } catch (error) {
